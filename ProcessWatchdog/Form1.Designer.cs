eb7928513f722479ae757030ae082b183d5013aa
Rogosch <d.rogosch@schulze-brakel.de>
2020-01-22 13:03:03 +0100

    Initial commit of ProcessWatchdog

---
+++ ProcessWatchdog/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ProcessWatchdog {
+    partial class Form1 {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ProcessWatchdog";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(155, 30);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "ProcessWatchdog";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ProcessWatchdog/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ProcessWatchdog {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void watchdogThread() {
+            String processName = null;
+            String programPath = null;
+            try {
+                using (StreamReader sr = new StreamReader("ProcessWatchdog.txt")) {
+                    String line;
+                    while ((line = sr.ReadLine()) != null) {
+                        if (!line.StartsWith("#")) {
+                            if (line.StartsWith("Processname")) {
+                                String[] nameParts = line.Split('=');
+                                processName = nameParts[1].Trim();
+                            }
+                            if (line.StartsWith("Path")) {
+                                String[] pathParts = line.Split('=');
+                                programPath = pathParts[1].Trim();
+                            }
+                        }
+                    }
+                }
+                if (processName != null && programPath != null) {
+                    while (true) {
+                        Thread.Sleep(500);
+                        Process[] processlist = Process.GetProcesses();
+                        bool isRunning = false;
+                        foreach (Process theprocess in processlist) {
+                            if (theprocess.ProcessName.ToLower().Contains(processName.ToLower())) {
+                                isRunning = true;
+                            }
+                        }
+                        if (!isRunning) {
+                            Process.Start(programPath);
+                        }
+                    }
+                }
+                else {
+                    MessageBox.Show("Fehler beim Initialisieren");
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Fehler: " + ex.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            Thread t1 = new Thread(new ThreadStart(watchdogThread));
+            t1.Start();
+        }
+    }
+}
